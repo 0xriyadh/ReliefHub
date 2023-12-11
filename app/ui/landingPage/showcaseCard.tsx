4ccc25db908cfc7f9207d6de9dbf78a83a2b9b8d
unknown <khan.md.saifullah@g.bracu.ac.bd>
2023-12-11 11:11:29 +0600

    minor improvements

--- app/ui/landingPage/showcaseCard.tsx
+++ app/ui/landingPage/showcaseCard.tsx
@@ -9,10 +9,10 @@ interface ShowcaseCardProps {
 }
 const ShowcaseCard: React.FC<ShowcaseCardProps> = ({ title, subtitle, imgSrc }) => {
     return (
-        <div className='flex flex-col mt-20 mx-10 p-6 bg-blue-100 shadow-md block rounded justify-center px-14 items-center space-x-4 w-[100%]'>
+        <div className='flex flex-col mt-20 mx-10 p-6 bg-blue-100 shadow-md rounded justify-center px-14 items-center space-x-4 w-[100%]'>
             <div className='flex flex-col space-y-4 '>
                 <Image src={imgSrc} alt='product'
-                    className="h-auto w-100" objectFit='cover' height={512} width={512}></Image>
+                    className="h-auto w-100" style={{ objectFit: "cover" }} height={512} width={512}></Image>
                 <div className='text-2xl font-bold text-black'>{title}</div>
                 <div className='text-lg font-semibold text-grey-800'>{subtitle}</div>
 
