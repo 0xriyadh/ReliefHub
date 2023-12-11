8ed7170409463ef73d0e9bcf7df19c112bbf04ae
Md. Mahadi Hassan Riyadh <mahadihassanriyadh1@gmail.com>
2023-12-11 20:16:23 +0600

    volunteers in team showing successfully
    Signed-off-by: Md. Mahadi Hassan Riyadh <mahadihassanriyadh1@gmail.com>

--- app/admin/team/[teamId]/page.tsx
+++ app/admin/team/[teamId]/page.tsx
@@ -3,31 +3,19 @@ import { TableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchUsersPages } from '@/app/lib/data';
 import MembersTable from '@/app/ui/members/table';
+import VolunteersTable from '@/app/ui/team/volunteers-table';
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams?: {
-    query?: string;
-    page?: string;
-  };
-}) {
-  const query = searchParams?.query || '';
-  const currentPage = Number(searchParams?.page) || 1;
-
-  const totalPages = await fetchUsersPages(query);
+export default async function Page({ params }: { params: { teamId: string } }) {
+  const teamId = params.teamId;
 
   return (
-    <div className="w-full mt-8">
+    <div className="mt-8 w-full">
       <div className="flex w-full items-center justify-between">
         <h1 className="text-2xl">Manage Volunteers</h1>
       </div>
-      <Suspense key={query + currentPage} fallback={<TableSkeleton />}>
-        <MembersTable query={query} currentPage={currentPage} />
+      <Suspense key={teamId} fallback={<TableSkeleton />}>
+        <VolunteersTable teamId={teamId} />
       </Suspense>
-      <div className="mt-5 flex w-full justify-center">
-        <Pagination totalPages={totalPages} />
-      </div>
     </div>
   );
 }
---
+++ app/ui/team/volunteers-table.tsx
@@ -0,0 +1,64 @@
+import { UpdateCampaign, DeleteCampaign } from '@/app/ui/campaigns/buttons';
+import Status from '@/app/ui/members/status';
+import { fetchVolunteersByTeamId } from '@/app/lib/data';
+import Link from 'next/link';
+
+export default async function VolunteersTable({ teamId }: { teamId: string }) {
+  const volunteers = await fetchVolunteersByTeamId(teamId);
+
+  return (
+    <div className="mt-6 flow-root">
+      <div className="inline-block min-w-full align-middle">
+        <div className="bg-gray-50 p-2 md:pt-0">
+          <div className="md:hidden">
+            kindly use a desktop to view the table
+          </div>
+          <table className="hidden min-w-full text-gray-900 md:table">
+            <thead className="text-left text-sm font-normal">
+              <tr>
+                <th scope="col" className="px-4 py-5 font-medium sm:pl-6">
+                  Name
+                </th>
+                <th scope="col" className="px-3 py-5 font-medium">
+                  Email
+                </th>
+                <th scope="col" className="px-3 py-5 font-medium">
+                  Phone
+                </th>
+                <th scope="col" className="relative py-3 pl-6 pr-3">
+                  <span className="sr-only">Edit</span>
+                </th>
+              </tr>
+            </thead>
+            <tbody className="bg-white">
+              {volunteers?.map((volunteer) => (
+                <tr
+                  key={volunteer.id}
+                  className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg"
+                >
+                  <td className="whitespace-nowrap py-3 pl-6 pr-3">
+                    <div className="flex items-center gap-3">
+                      <Link href={`/admin/campaigns/${volunteer.id}`}>
+                        <p className="hover:text-primary-color-600">
+                          {volunteer.name}
+                        </p>
+                      </Link>
+                    </div>
+                  </td>
+                  <td className="whitespace-nowrap px-3 py-3">{volunteer.email}</td>
+                  <td className="whitespace-nowrap px-3 py-3">{volunteer.phone}</td>
+                  <td className="whitespace-nowrap py-3 pl-6 pr-3">
+                    <div className="flex justify-end gap-3">
+                      {/* <UpdateCampaign id={volunteer.id} />
+                      <DeleteCampaign id={volunteer.id} /> */}
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+}
