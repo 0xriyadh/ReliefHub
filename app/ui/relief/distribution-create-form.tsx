beaadd12e91e89444e02b2bf3e4bc238d0305aec
Md. Mahadi Hassan Riyadh <mahadihassanriyadh1@gmail.com>
2023-12-11 06:53:23 +0600

    distributing reliefs successfully
    Signed-off-by: Md. Mahadi Hassan Riyadh <mahadihassanriyadh1@gmail.com>

--- app/ui/relief/distribution-create-form.tsx
+++ app/ui/relief/distribution-create-form.tsx
@@ -13,7 +13,7 @@ import {
   ReliefRecipientForDistribution,
   ReliefStocksField,
 } from '@/app/lib/definitions';
-import { createCampaignTeam } from '@/app/lib/actions';
+import { createCampaignTeam, createDistributeRelief } from '@/app/lib/actions';
 
 export default function CreateDistributionForm({
   reliefId,
@@ -23,35 +23,15 @@ export default function CreateDistributionForm({
   reliefId: string;
   recipients: ReliefRecipientForDistribution[];
   reliefStocks: ReliefStocksField[];
-}) {
+  }) {
+  const distributeRelief = createDistributeRelief.bind(
+    null,
+    reliefId,
+    reliefStocks,
+  );
   return (
-    <form action={createCampaignTeam}>
+    <form action={distributeRelief}>
       <div className="bg-gray-50 p-4 md:p-6">
-        {/* Relief */}
-        <div className="mb-4">
-          <label
-            htmlFor="relief-name"
-            className="mb-2 block text-sm font-medium"
-          >
-            Relief ID
-          </label>
-          <div className="relative mt-2">
-            <div className="relative">
-              <input
-                id="relief-id"
-                name="reliefId"
-                type="text"
-                step="0.01"
-                placeholder="Give your campaign a unique name"
-                className="peer block w-full border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-                aria-describedby="relief-id-error"
-                value={reliefId}
-              />
-              <MegaphoneIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-            </div>
-          </div>
-        </div>
-
         {/* Relief Recipient */}
         <div className="mb-4">
           <label htmlFor="recipient" className="mb-2 block text-sm font-medium">
@@ -127,11 +107,10 @@ export default function CreateDistributionForm({
             <CalculatorIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
           </div>
         </div>
-
       </div>
       <div className="mt-6 flex justify-end gap-4">
         <Link
-          href={`/admin/relief/${reliefId}/distribution`}
+          href={`/admin/relief/${reliefId}/distributions`}
           className="flex h-10 items-center bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
         >
           Cancel
